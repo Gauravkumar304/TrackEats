1d729de520434b429c8cf8269b6060cd87dcc09a
Gauravkumar304 <pushkarkumarsingh7499@gmail.com>
2024-10-14 09:51:55 +0530

    fooditemlist

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,24 @@
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import FoodListItem from "@/components/FoodListItem";
 
-export default function App(){
+
+export default function App() {
   return (
-    <View style = {styles.container}>
-      <Text>Hello There from expo Router</Text>
-    </View>
+    <View style={styles.container}>
+      {/* food item view */}
+      <FoodListItem item={{label:"Pizza", cal: 75, brand: 'Dominos'}}  />
+      <FoodListItem item={{label:"Apple", cal: 50, brand: 'Generic'}}  />
+     
+      </View>  
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    padding: 10,
+    gap:5,
   },
 });
